e46e817e554d3929a9862b7c296ee1b9a62446c4
Soraya Costa Soaresr <soares.s@aluno.ifsp.edu.br>
2025-05-31 17:54:13 -0300

    [Main Lading Page] Footer + Media Query Fixes

--- arte_eliane/js/script.js
+++ arte_eliane/js/script.js
@@ -10,3 +10,33 @@ document.addEventListener("DOMContentLoaded", () => {
     });
   });
 });
+
+
+document.addEventListener('DOMContentLoaded', function() {
+  const menuToggle = document.querySelector('.menu-toggle');
+  const nav = document.querySelector('.nav');
+
+  if (nav) {
+    nav.classList.remove('open');
+  }
+
+  if (menuToggle && nav) {
+    menuToggle.addEventListener('click', function() {
+      nav.classList.toggle('open');
+    });
+
+    // Fecha o menu ao clicar em um link (opcional)
+    nav.querySelectorAll('li').forEach(link => {
+      link.addEventListener('click', () => {
+        nav.classList.remove('open');
+      });
+    });
+
+    // Fecha o menu ao clicar fora dele
+    document.addEventListener('click', (event) => {
+      if (!nav.contains(event.target) && !menuToggle.contains(event.target)) {
+        nav.classList.remove('open');
+      }
+    });
+  }
+});
